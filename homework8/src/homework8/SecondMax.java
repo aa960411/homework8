469927a398a6d8734abc309b17164543e12eb8cc
hyein <aa960411@naver.com>
2016-11-06 19:03:37 +0900

    숫자 입력 X 오류

---
+++ homework8/src/homework8/SecondMax.java
@@ -0,0 +1,33 @@
+package homework8;
+
+import java.util.Scanner;
+
+public class SecondMax {
+	private double[] num;
+	private int len;
+	
+	void start() {
+		num = new double[10];
+		len = num.length;
+		this.input();
+		this.print();
+	}	
+	
+
+
+	void input() {
+		Scanner s = new Scanner(System.in);
+		double[] num = new double[10];
+		String inputNumber;
+		int i = 0;
+		System.out.print("수를 입력하세요. : ");
+
+	
+		System.out.print("\n");
+	}
+	
+	void print() {
+	}
+	
+	
+}
