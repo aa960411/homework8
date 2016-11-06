eac95cc126cd9cbc45c682261dad04348c7db3e3
hyein <aa960411@naver.com>
2016-11-06 19:13:33 +0900

    1번 성공 (입력 오류 해결)

--- homework8/src/homework8/SecondMax.java
+++ homework8/src/homework8/SecondMax.java
@@ -1,33 +1,50 @@
 package homework8;
-
-import java.util.Scanner;
+import java.util.*;
 
 public class SecondMax {
-	private double[] num;
+	private int[] numArr;
 	private int len;
 	
 	void start() {
-		num = new double[10];
-		len = num.length;
+		numArr = new int[5];
+		len = numArr.length;
 		this.input();
 		this.print();
-	}	
-	
-
-
+	}
 	void input() {
 		Scanner s = new Scanner(System.in);
-		double[] num = new double[10];
-		String inputNumber;
-		int i = 0;
-		System.out.print("수를 입력하세요. : ");
-
-	
+		System.out.println("입력 받은 숫자 중 두번째로 큰 숫자를 구한다.");
+		
+		
+		for(int i=0;i<len;i++) {
+			System.out.print(String.format("%02d", i+1) + "번째 수를 입력하시오: ");
+			
+			if(s.hasNextInt())
+				numArr[i] = s.nextInt();
+			else
+				return;
+		}
+		
 		System.out.print("\n");
 	}
-	
 	void print() {
+		int max = Integer.MIN_VALUE;
+		int secondmax = Integer.MIN_VALUE;
+		int maxindex = 0, secondmaxindex = 0;
+		
+		for(int i=0;i<len;i++) {
+			if(numArr[i] > max) {
+				secondmax = max;
+				secondmaxindex = maxindex;
+				max = numArr[i];
+				maxindex = i;
+			}
+			else if(numArr[i] > secondmax) {
+				secondmax = numArr[i];
+				secondmaxindex = i;
+			}
+		}
+		
+		System.out.println("두 번째로 큰 수는 " + (secondmaxindex+1) + "번째수 " + secondmax +" 입니다. \n");
 	}
-	
-	
 }
